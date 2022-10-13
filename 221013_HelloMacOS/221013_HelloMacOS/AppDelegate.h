2a4f5c4f62aa34f5051efd9fe7e4d883c62b74d2
k906506 <k906506@naver.com>
2022-10-13 09:48:12 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221013_HelloMacOS/221013_HelloMacOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  221013_HelloMacOS
+//
+//  Created by 고도 on 2022/10/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ 221013_HelloMacOS/221013_HelloMacOS/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  221013_HelloMacOS
+//
+//  Created by 고도 on 2022/10/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
