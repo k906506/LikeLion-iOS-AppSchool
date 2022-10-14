ab39e1865841c804f57c27a378376fb6071c31a2
k906506 <k906506@naver.com>
2022-10-15 00:34:08 +0900

    [feat] 사칙연산 및 연산자, 등호 연속으로 눌렀을 경우에도 계산이 되도록 구현

--- 221014_RealCalculater/221014_RealCalculater/ViewController.swift
+++ 221014_RealCalculater/221014_RealCalculater/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var firstOperand: Float = 0
     var secondOperand: Float = 0
+    var op: String = ""
     var number: String = ""
     
     override func viewDidLoad() {
@@ -35,11 +36,18 @@ class ViewController: UIViewController {
     // 피연산자가 두 개인 연산자
     @IBAction func tapOperatorButton(_ sender: UIButton) {
         // 옵셔널 바인딩 적용
+        if let optionalOp = sender.titleLabel?.text {
+            op = optionalOp
+        }
+        // 옵셔널 바인딩 적용
         if let num = Float(number) {
-            firstOperand = num
+            if (firstOperand == 0) {
+                firstOperand = num
+            }
         }
         // 숫자를 저장하기 위한 문자열 초기화
         number = ""
+        self.resultLabel.text = number
     }
     
     // 피연산자가 한 개인 연산자
@@ -55,7 +63,7 @@ class ViewController: UIViewController {
         switch op {
         case "+/-":
             temp = "\(firstOperand * -1)"
-  
+            
         default:
             temp = "\(firstOperand * 0.01)"
         }
@@ -67,10 +75,56 @@ class ViewController: UIViewController {
     
     // 결과, 삭제
     @IBAction func tapCalculateOrClearButton(_ sender: UIButton) {
-        number = ""
-        firstOperand = 0
-        secondOperand = 0
-        self.resultLabel.text = number
+        guard let button = sender.titleLabel?.text else { return }
+        
+        switch button {
+        case "AC" :
+            number = ""
+            firstOperand = 0
+            secondOperand = 0
+            self.resultLabel.text = number
+        default:
+            // 옵셔널 바인딩 적용
+            if let num = Float(number) {
+                secondOperand = num
+                
+            }
+            
+            number = calculate(number1: firstOperand, number2: secondOperand, op: op)
+            self.resultLabel.text = number
+            
+            // = 버튼만 눌러도 한번 더 연산되도록 피연산자 수정
+            // 옵셔널 바인딩 적용
+            if let num = Float(number) {
+                firstOperand = num
+            }
+            
+            number = "\(secondOperand)"
+        }
+    }
+    
+    func calculate(number1: Float, number2: Float, op: String) -> String {
+        var temp: String = ""
+        
+        switch op {
+        case "+":
+            temp = "\(number1 + number2)"
+        case "-":
+            temp = "\(number1 - number2)"
+        case "X":
+            temp = "\(number1 * number2)"
+        case "/":
+            if number2 != 0 {
+                temp = "\(number1 / number2)"
+            } else {
+                // 예외 처리
+                debugPrint("예외 처리")
+            }
+        default:
+            // 예외 처리
+            debugPrint("예외 처리")
+        }
+        
+        return temp
     }
 }
-
