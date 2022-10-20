cdc8d23f8b6c8bf394cdc37da478f6d8e53c59a2
k906506 <k906506@naver.com>
2022-10-20 23:50:35 +0900

    [feat] 프로젝트 초기 세팅

---
+++ 221020_randomNumber/221020_randomNumber/_21020_randomNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21020_randomNumberApp.swift
+//  221020_randomNumber
+//
+//  Created by 고도 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct _21020_randomNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
