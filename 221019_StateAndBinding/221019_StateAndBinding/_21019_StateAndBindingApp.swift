eb55fcc1b1ed37f724003d020991e21e9712043f
k906506 <k906506@naver.com>
2022-10-21 22:49:27 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221019_StateAndBinding/221019_StateAndBinding/_21019_StateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21019_StateAndBindingApp.swift
+//  221019_StateAndBinding
+//
+//  Created by 고도 on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct _21019_StateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
