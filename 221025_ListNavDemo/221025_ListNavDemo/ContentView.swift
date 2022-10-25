3c363cd0bfc2e34c3b9f91689c0722c8dbc5ff60
k906506 <k906506@naver.com>
2022-10-25 13:25:12 +0900

    [feat] NavigationView 활용해서 세부 정보 페이지로 이동하도록 구현, Delete, Move 메소드 구현

--- 221025_ListNavDemo/221025_ListNavDemo/ContentView.swift
+++ 221025_ListNavDemo/221025_ListNavDemo/ContentView.swift
@@ -12,11 +12,32 @@ struct ContentView: View {
     @ObservedObject var carStore: CarStore = CarStore(cars: carData)
     
     var body: some View {
-        List {
-            ForEach (carStore.cars) { car in
-                ListCell(car: car)
-            }.listStyle(.plain)
+        NavigationView {
+            List {
+                ForEach (carStore.cars) { car in
+                    ListCell(car: car)
+                }
+                // .listStyle(.plain)
+                .onDelete(perform: deleteItems)
+                .onMove(perform: moveItems)
+            }
+            .navigationTitle(Text("EV Cars"))
+            .navigationBarItems(
+                leading: NavigationLink(
+                    destination: AddNewCar(carStore: self.carStore)) {
+                        Text("Add")
+                    }
+                ,trailing: EditButton()
+            )
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        carStore.cars.remove(atOffsets: offsets)
+    }
+    
+    func moveItems(from source: IndexSet, to destination: Int) {
+        carStore.cars.move(fromOffsets: source, toOffset: destination)
     }
 }
 
@@ -30,14 +51,16 @@ struct ListCell: View {
     var car: Car
     
     var body: some View {
-        HStack {
-            Image(car.imageName)
-                .resizable()
-                .cornerRadius(12)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-            Text(car.name)
-                .padding()
+        NavigationLink(destination: CarDetail(selectedCar: car)) {
+            HStack {
+                Image(car.imageName)
+                    .resizable()
+                    .cornerRadius(12)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text(car.name)
+                    .padding()
+            }
         }
     }
 }
