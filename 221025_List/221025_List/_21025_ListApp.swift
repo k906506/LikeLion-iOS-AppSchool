7d80e5234f23a3b1f98eed150268336136968b80
k906506 <k906506@naver.com>
2022-10-25 10:12:42 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221025_List/221025_List/_21025_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21025_ListApp.swift
+//  221025_List
+//
+//  Created by 고도 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _21025_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
