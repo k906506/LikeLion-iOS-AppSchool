dda6b26bed9ff6a78966e6d2ed75efad4cb982ba
k906506 <k906506@naver.com>
2022-10-25 11:50:36 +0900

    [feat] 차량 세부 정보 페이지 추가

---
+++ 221025_ListNavDemo/221025_ListNavDemo/CarDetail.swift
@@ -0,0 +1,43 @@
+//
+//  CarDetail.swift
+//  221025_ListNavDemo
+//
+//  Created by 고도 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct CarDetail: View {
+    let selectedCar: Car
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Car Details")) {
+                Image(selectedCar.imageName)
+                    .resizable()
+                    .cornerRadius(12)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Text(selectedCar.name)
+                    .font(.headline)
+                
+                Text(selectedCar.description)
+                    .font(.body)
+                
+                HStack {
+                    Text("Hybrid")
+                        .font(.headline)
+                        Spacer()
+                    Image(systemName: selectedCar.isHybrid ? "checkmark.circle" : "xmark.circle")
+                }
+            }
+        }
+    }
+}
+
+struct CarDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetail(selectedCar: carData[0])
+    }
+}
