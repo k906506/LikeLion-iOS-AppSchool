7730941b368651048b2b5b8c6d650f01a819f5cd
k906506 <k906506@naver.com>
2022-10-25 11:51:12 +0900

    [feat] 차량 목록 보여주는 뷰 추가

--- 221025_ListNavDemo/221025_ListNavDemo/ContentView.swift
+++ 221025_ListNavDemo/221025_ListNavDemo/ContentView.swift
@@ -6,21 +6,38 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @ObservedObject var carStore: CarStore = CarStore(cars: carData)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach (carStore.cars) { car in
+                ListCell(car: car)
+            }.listStyle(.plain)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListCell: View {
+    var car: Car
+    
+    var body: some View {
+        HStack {
+            Image(car.imageName)
+                .resizable()
+                .cornerRadius(12)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+            Text(car.name)
+                .padding()
+        }
     }
 }
