41c8d21eaf4234c13212782d60671441d20ade56
k906506 <k906506@naver.com>
2022-10-27 16:32:07 +0900

    [update] 프로젝트 기본 세팅

---
+++ 221027_Drawing/221027_Drawing/_21027_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21027_DrawingApp.swift
+//  221027_Drawing
+//
+//  Created by 고도 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct _21027_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
