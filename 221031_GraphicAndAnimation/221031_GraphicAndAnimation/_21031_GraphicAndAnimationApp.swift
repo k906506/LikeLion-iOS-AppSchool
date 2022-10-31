ebbd09f83df814f564420d72836a923e1b5872c0
k906506 <k906506@naver.com>
2022-10-31 13:10:39 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221031_GraphicAndAnimation/221031_GraphicAndAnimation/_21031_GraphicAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21031_GraphicAndAnimationApp.swift
+//  221031_GraphicAndAnimation
+//
+//  Created by 고도 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct _21031_GraphicAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
