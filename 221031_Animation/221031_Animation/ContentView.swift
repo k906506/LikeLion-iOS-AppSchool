41a8e2fa74adbb8a109bdeca64297baf9e1ad531
k906506 <k906506@naver.com>
2022-10-31 10:27:31 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221031_Animation/221031_Animation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  221031_Animation
+//
+//  Created by 고도 on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 221031_Animation/221031_Animation/_21031_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21031_AnimationApp.swift
+//  221031_Animation
+//
+//  Created by 고도 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct _21031_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
