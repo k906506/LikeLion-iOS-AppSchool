c7ddb9370726984d4e6f8fadfda3c93aae9e5a6e
k906506 <k906506@naver.com>
2022-11-01 09:26:28 +0900

    [update] 프로젝트 기본 세팅

---
+++ 221101_Gesture/221101_Gesture/_21101_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21101_GestureApp.swift
+//  221101_Gesture
+//
+//  Created by 고도 on 2022/11/01.
+//
+
+import SwiftUI
+
+@main
+struct _21101_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
