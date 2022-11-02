3ec85367eb01a57c4e74ed769282d27bbbe7cba0
k906506 <k906506@naver.com>
2022-11-02 09:25:52 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221102_IntegrateSwiftUIAndUIView/221102_IntegrateSwiftUIAndUIView/_21102_IntegrateSwiftUIAndUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21102_IntegrateSwiftUIAndUIViewApp.swift
+//  221102_IntegrateSwiftUIAndUIView
+//
+//  Created by 고도 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct _21102_IntegrateSwiftUIAndUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
