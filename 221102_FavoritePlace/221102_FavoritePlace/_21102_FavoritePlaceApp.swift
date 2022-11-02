ba17c185ea6c0ff72b7337123a1555ae58578cf0
k906506 <k906506@naver.com>
2022-11-02 22:18:17 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221102_FavoritePlace/221102_FavoritePlace/_21102_FavoritePlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21102_FavoritePlaceApp.swift
+//  221102_FavoritePlace
+//
+//  Created by 고도 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct _21102_FavoritePlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
