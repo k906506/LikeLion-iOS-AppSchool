877d88229541a8c1fa3567c2e78be18e2f61368c
k906506 <k906506@naver.com>
2022-11-16 11:22:44 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221116_SharedLink/221116_SharedLink/_21116_SharedLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21116_SharedLinkApp.swift
+//  221116_SharedLink
+//
+//  Created by 고도 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct _21116_SharedLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
