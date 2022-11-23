cb2058ecc74d53d5f68bec145734111e95a09b3f
k906506 <k906506@naver.com>
2022-11-23 16:10:34 +0900

    [feat] 데이터를 가져올 때 ProgressView가 보여지도록 구현

--- 221123_AsyncAwait/221123_AsyncAwait/View/LectureDetailView.swift
+++ 221123_AsyncAwait/221123_AsyncAwait/View/LectureDetailView.swift
@@ -11,35 +11,40 @@ struct LectureDetailView: View {
     @EnvironmentObject var lectureVM: LectureViewModel
     
     var body: some View {
-        VStack {
-            List(lectureVM.items) { item in
-                LectureItemView(lecture: item)
-            }
-            .listStyle(.inset)
-            .task {
-                do {
-                    try await lectureVM.getLecturesOnServer()
-                } catch (let error) {
-                    print("Unable to get data : \(error)")
-                }
-            }
-            
-            Button(action: {
-                Task {
+        if lectureVM.items.isEmpty {
+            ProgressView()
+            // 앱을 처음 실행했을 때만 ProgressView를 보여줌
+                .task {
                     do {
-                        try await lectureVM.getLecturesOnServerAtFinishedScroll()
+                        try await lectureVM.getLecturesOnServer()
                     } catch (let error) {
                         print("Unable to get data : \(error)")
                     }
                 }
-            }) {
-                Text("강좌 가져오기")
+        } else {
+            VStack {
+                List(lectureVM.items) { item in
+                    LectureItemView(lecture: item)
+                }
+                .listStyle(.inset)
+                
+                
+                Button(action: {
+                    Task {
+                        do {
+                            try await lectureVM.getLecturesOnServerAtFinishedScroll()
+                        } catch (let error) {
+                            print("Unable to get data : \(error)")
+                        }
+                    }
+                }) {
+                    Text("강좌 가져오기")
+                }
             }
+            .navigationTitle("K-MOOC 강좌 목록")
         }
-        
-        .navigationTitle("K-MOOC 강좌 목록")
-        
     }
+    
 }
 
 //struct LectureDetailView_Previews: PreviewProvider {
