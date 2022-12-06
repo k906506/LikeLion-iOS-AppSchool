6325d9c62cf41d2a179bf7813a04aa96a7031e00
k906506 <k906506@naver.com>
2022-12-06 16:09:53 +0900

    [update] 프로젝트 초기 세팅

---
+++ 221206_FirebaseBasic/221206_FirebaseBasic/_21206_FirebaseBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  _21206_FirebaseBasicApp.swift
+//  221206_FirebaseBasic
+//
+//  Created by 고도 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct _21206_FirebaseBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
